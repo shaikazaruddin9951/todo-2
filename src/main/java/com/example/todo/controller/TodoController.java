6fef6fc1421bc7dfe2b98ede946575319b47b6a7
Mohammad Ajaruddin Shaik <shaikazaruddin9951@gmail.com>
2024-03-28 18:03:02 +0000

    solution

---
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,47 @@
+package com.example.todo.controller;
+
+import java.util.ArrayList;
+
+import com.example.todo.model.Todo;
+import com.example.todo.service.TodoH2Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+    @Autowired
+    private TodoH2Service todoService;
+
+    @GetMapping("/todos")
+    public ArrayList<Todo> getTodos() {
+        return todoService.getTodos();
+    }
+
+    @GetMapping("/todos/{id}")
+    public Todo getTodoById(@PathVariable("id") int id) {
+        return todoService.getTodoById(id);
+    }
+
+    @PostMapping("/todos")
+    public Todo addTodo(@RequestBody Todo todo) {
+        return todoService.addTodo(todo);
+    }
+
+    @PutMapping("/todos/{id}")
+    public Todo updateTodo(@PathVariable("id") int id, @RequestBody Todo todo) {
+        return todoService.updateTodo(id, todo);
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public void deleteTodo(@PathVariable int id) {
+        todoService.deleteTodo(id);
+    }
+
+}
